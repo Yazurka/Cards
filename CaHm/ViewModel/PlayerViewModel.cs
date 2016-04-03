221358af2686cc128c4b491e73ab54279db6fb34
Marius Lian <marius_lian@hotmail.com>
2016-04-03 14:38:38 +0200

    added some basic clases for further work

---
+++ CaHm/ViewModel/PlayerViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaHm.ViewModel
+{
+    public class PlayerViewModel : ViewModelBase
+    {
+        private string m_name;
+        private int m_score;
+
+        public string Name
+        {
+            get
+            {
+                return m_name;
+            }
+            set
+            {
+                m_name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public int Score
+        {
+            get
+            {
+                return m_score;
+            }
+            set
+            {
+                m_score = value;
+                OnPropertyChanged("Score");
+            }
+        }
+    }
+}
---
+++ CaHm/ViewModel/ScoreboardViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaHm.ViewModel
+{
+    public class ScoreboardViewModel : ViewModelBase
+    {
+        private ObservableCollection<PlayerViewModel> m_players;
+
+        public ObservableCollection<PlayerViewModel> Players
+        {
+            get
+            {
+                return m_players;
+            }
+            set
+            {
+                m_players = value;
+                OnPropertyChanged("Players");
+            }
+        }
+    }
+}
