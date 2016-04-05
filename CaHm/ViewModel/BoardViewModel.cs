204a2ef0e00c136af32d24fe3735d8dfa7643022
Marius Lian <marius_lian@hotmail.com>
2016-04-05 23:34:44 +0200

    Got connection

--- CaHm/ViewModel/BoardViewModel.cs
+++ CaHm/ViewModel/BoardViewModel.cs
@@ -6,23 +6,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.AspNet.SignalR.WebSockets;
+using System.Net;
 
 namespace CaHm.ViewModel
 {
     public class BoardViewModel : ViewModelBase, IDropTarget
     {
+        private HubConnection m_hubConnection;
+        private IHubProxy m_hubProxy;
         private BlackCardViewModel m_blackCardViewModel;
         private ObservableCollection<WhiteCardViewModel> m_cardsOnHand;
         private ObservableCollection<WhiteCardViewModel> m_cardsOnBoard;
-        public BoardViewModel()
+        public  BoardViewModel()
         {
             CardsOnBoard = new ObservableCollection<WhiteCardViewModel>();
             BlackCardViewModel = new BlackCardViewModel();
             generateWhitecardsOnHand();
             generateWhitecardsOnBoard();
+            //try
+            //{
+                var querystringData = new Dictionary<string, string>();
+                querystringData.Add("name", "asad");
+                querystringData.Add("message", "sdfsdsd");
+                m_hubConnection = new HubConnection("http://localhost:8080", querystringData);
+                m_hubProxy = m_hubConnection.CreateHubProxy("MyHub");
+                m_hubProxy.On<string, string>("Send", lol);
+            
+                ServicePointManager.DefaultConnectionLimit = 1;
+                m_hubConnection.Start().Wait();
+                //m_hubProxy.On<string, string>("Send", (name, message) =>
+                //{
+                //    MessageBox.Show("Name " + name + " Message " + message);
+                //});
+              
+                m_hubConnection.Received += m_hubConnection_Received;
+            //}catch(Exception ex){
+            //    throw ex;
+            //}
+            
         }
-        void IDropTarget.DragOver(IDropInfo dropInfo)
+        private void lol(string name, string message)
         {
+
+        }
+        private void m_hubConnection_Received(string obj)
+        {
+            MessageBox.Show("Recived");
+        }
+         void IDropTarget.DragOver(IDropInfo dropInfo)
+        {
+
+           
             WhiteCardViewModel sourceItem = dropInfo.Data as WhiteCardViewModel;
             WhiteCardViewModel targetItem = dropInfo.TargetItem as WhiteCardViewModel;
 
@@ -35,6 +71,9 @@ namespace CaHm.ViewModel
 
         void IDropTarget.Drop(IDropInfo dropInfo)
         {
+            //var querystringData = new Dictionary<string, string>();
+            //querystringData.Add("contosochatversion", "1.0");
+            //m_hubConnection.Send(querystringData).Wait();
             WhiteCardViewModel sourceItem = dropInfo.Data as WhiteCardViewModel;
             WhiteCardViewModel targetItem = dropInfo.TargetItem as WhiteCardViewModel;
             this.CardsOnBoard.Add(sourceItem);
---
+++ CaHm/ViewModel/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CaHm.ViewModel
+{
+    public class Message
+    {
+        public string Symbol { get; set; }
+        public decimal Price { get; set; }
+    }
+}
