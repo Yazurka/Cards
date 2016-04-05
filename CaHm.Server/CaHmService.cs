1369cb20e67e090199063599645ac45a811cef39
Marius Lian <marius_lian@hotmail.com>
2016-04-05 22:11:09 +0200

    Added Server project as a Service. initial

---
+++ CaHm.Server/CaHmService.cs
@@ -0,0 +1,55 @@
+﻿using System.ServiceProcess;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Hosting;
+using Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin;
+
+
+namespace CaHm.Server
+{
+    public partial class CaHmService : ServiceBase
+    {
+        public CaHmService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+
+            if (!System.Diagnostics.EventLog.SourceExists("CaHmService"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "CaHmService", "Application");
+            }
+            eventLog1.Source = "CaHmService";
+            eventLog1.Log = "Application";
+
+            eventLog1.WriteEntry("In OnStart");
+            string url = "http://localhost:8080";
+            WebApp.Start(url);
+
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("In OnStop");
+        }
+    }
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+    public class MyHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.addMessage(name, message);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Cards
+## Install Service
+cd C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\Shortcuts
+start Developer Command Prompt for VS2013
+type "installutil CaHm.Server.exe" into the commandline
+
+The service should now be installed
+
+## Start Service
+Open "Services" app from pressing the windows button and start typing "Services"
+Right click CaHmServerService and start
