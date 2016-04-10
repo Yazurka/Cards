84f6831ef6d90e9a7767d93b2fd3ad5e62a08ab8
Frederik91 <frederik.t91@gmail.com>
2016-04-10 02:28:44 +0200

    Basic webpage sketch

---
+++ WebApplication4/script.js
@@ -0,0 +1,62 @@
+﻿// script.js
+
+// create the module and name it scotchApp
+// also include ngRoute for all our routing needs
+var scotchApp = angular.module('scotchApp', ['ngRoute']);
+
+// configure our routes
+scotchApp.config(function ($routeProvider) {
+    $routeProvider
+
+        // route for the username page
+        .when('/', {
+            templateUrl: 'pages/usernamePage.html',
+            controller: 'usernameController'
+        })
+
+        // route for the startpage
+        .when('/startpage', {
+            templateUrl: 'pages/startpage.html',
+            controller: 'startpageController'
+        })
+
+        // route for the game lobby page
+        .when('/gameLobby/:id', {
+            templateUrl: 'pages/gameLobbyPage.html',
+            controller: 'gameLobbyController'
+        });
+});
+
+scotchApp.controller('mainController', function ($scope) {
+    $scope.$on('usernameEvent', function (event, data) {
+        $scope.username = data;
+    });
+    $scope.$on('gameIdEvent', function (event, data) {
+        $scope.gameID = data;
+    });
+});
+
+//create the controller and inject Angular's $scope
+scotchApp.controller('usernameController', function ($scope, $routeParams, $location) {
+    // create a message to display in our view
+    $scope.username = "";
+    $scope.submitUsername = function () {
+        $scope.$emit('usernameEvent', $scope.username);
+        $location.url('/startpage')
+    };
+});
+
+scotchApp.controller('startpageController', function ($scope, $routeParams, $location) {
+    $scope.createGame = function () {
+        $location.url('/gameLobby/' + 'sexualTension')
+    };
+    $scope.joinGame = function () {
+        $scope.$emit('gameIdEvent', $scope.gameID);
+        $location.url('/gameLobby/' + $scope.gameID);
+    };
+});
+
+scotchApp.controller('gameLobbyController', function ($scope, $http, $routeParams) {
+    $scope.gameID = $routeParams.id;
+});
+
